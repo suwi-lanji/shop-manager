defacba60a4860dd39f45ce2cbfaa45988b0e1d9
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-01-13 13:44:24 +0200

    create credit table

---
+++ database/migrations/2025_01_13_114223_create_credits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('credits', function (Blueprint $table) {
+            $table->id();
+            $table->string('debtor_name');
+            $table->double('amount');
+            $table->string('status');
+            $table->string('description');
+            $table->foreignId('store_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('credits');
+    }
+};
