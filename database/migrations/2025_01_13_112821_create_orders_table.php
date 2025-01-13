fd5f0a09d211e49c5e78fe10e810bcd0b02613e0
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-01-13 13:41:33 +0200

    add store id to order

--- database/migrations/2025_01_13_112821_create_orders_table.php
+++ database/migrations/2025_01_13_112821_create_orders_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('order_number');
             $table->string('status');
             $table->double('amount');
+            $table->foreignId('store_id');
             $table->timestamps();
         });
     }
