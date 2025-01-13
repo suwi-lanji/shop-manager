d982b70a337c21be8849ba4d04d8779bd22c61de
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-01-13 13:37:06 +0200

    add order product model relation

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_product');
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     {
         return $this->belongsTo(Store::class);
     }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'order_product');
+    }
 }
