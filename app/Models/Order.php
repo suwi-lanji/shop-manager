f4af87c3dc83a5254326d1ff1dc4412302f25004
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-01-13 18:46:55 +0200

    added orders resource

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $guarded = [];
     public function products()
     {
         return $this->belongsToMany(Product::class, 'order_product');
